faf561ae9e298809b38d43bb03f4f980708c5f5d
Ale Muñoz <bomberstudios@gmail.com>
2022-08-01 12:23:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,54 +3,62 @@ const fetch = require("node-fetch");
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", function (request, response) {
   response.send("Sketch Workspace Webhook Demo");
 });
 
 app.post("/", function (request, response) {
-  console.log("POST received from Sketch Workspace. Analyzing last update:");
+  console.log("POST received from Sketch Workspace. Yay 🎉");
+
   // We can now download the document from the workspace
-  const SKETCH_TOKEN = process.env.SKETCH_TOKEN;
-  const WORKSPACE_ID = process.env.WORKSPACE_ID;
-  const DOCUMENT_ID = process.env.DOCUMENT_ID;
+  const SKETCH_TOKEN = process.env.SECRET;
+  const WORKSPACE_ID = process.env.WS_ID;
+  const DOCUMENT_ID = process.env.DOC_ID;
 
-  client.getDocuments(WORKSPACE_ID_CSM, SKETCH_TOKEN).then((docArray) => {
-    //console.log(`${docArray.length} documents in Workspace`)
+  //console.log(request.body);
+
+  client.getDocuments(WORKSPACE_ID, SKETCH_TOKEN).then((docArray) => {
+    console.log(`There are ${docArray.length} documents in your workspace`);
     const doc = docArray.find((doc) => doc.id == DOCUMENT_ID);
-    if (doc._links && doc._links.updates && doc._links.updates.url) {
-      fetch(doc._links.updates.url, {
-        headers: {
-          Authorization: `Basic ${SKETCH_TOKEN}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((json) => {
-          // This will print the list of updates for this document.
-          // console.log(json.data)
-          // Most of them will have a 'state' attribute that's set to 'FINISHED',
-          // and the last one will have a 'state' attribute that's set to 'PROCESSING'
-          // let's go with the last one that's FINISHED:
-          const lastUpdate = json.data.pop();
-          //const lastUpdate = json.data.reverse().find(update => update.state == 'FINISHED')
-          console.log(lastUpdate);
-
-          // Let's ask the Workspace for the exportable assets:
-          const exportableAssetsURL = lastUpdate._links.export.url;
-          // We'll make a POST request to that URL, and wait for a response from the server (this may take a while)
-          console.log(`Sending a POST request to ${exportableAssetsURL}`)
-          // fetch(exportableAssetsURL, {
-          //   method: "POST",
-          //   headers: {
-          //     Authorization: `Basic ${SKETCH_TOKEN}`,
-          //   },
-          // })
-          //   .then((res) => res.json())
-          //   .then((json) => {
-          //     console.log(json);
-          //   });
-        });
-    }
+    console.log(
+      `We're interested in "${doc.name}", so let's check its updates:`
+    );
+    client.getUpdatesForDocument(doc, SKETCH_TOKEN).then((updates) => {
+      console.log(`Document has ${updates.length} updates.`);
+      const unprocessedUpdates = updates.filter(
+        (update) => update.state == "PROCESSING"
+      );
+      const lastProcessedUpdate = updates
+          .reverse()
+          .find((update) => update.state === "FINISHED");
+      if (unprocessedUpdates.length > 0) {
+        console.log(`We haven't processed all updates`);
+        console.log(`So we need to ask the server to export the assets`)
+        // I still don't know what all of this means, to be honest
+        //const firstUnprocessedUpdate = unprocessedUpdates[0];
+        const exportableAssetsURL = lastProcessedUpdate._links.export.url;
+        console.log(`Sending a POST request to ${exportableAssetsURL}`);
+        fetch(exportableAssetsURL, {
+          method: "POST",
+          headers: {
+            Authorization: `Basic ${SKETCH_TOKEN}`,
+          },
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            console.log(json);
+          });
+      } else {
+        // All updates are processed, so we can download the last one:
+        console.log(`All updates are processed`);
+        console.log(
+          `We can now download the assets from ${lastProcessedUpdate._links.download.url}`
+        );
+        client.downloadUpdates(lastProcessedUpdate._links.download.url, './downloads', SKETCH_TOKEN)
+      }
+    });
   });
 });
 
